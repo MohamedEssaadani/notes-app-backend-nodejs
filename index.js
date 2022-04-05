748274410d95c59719efd5428722904c7fc95dba
Mohamed Es-saadani <essaadani80@gmail.com>
2022-04-05 15:11:04 +0000

    error middlewares

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDb from "./config/db.js";
-import noteRoutes from "./routes/noteRoutes.js";
-import categoryRoutes from "./routes/categoryRoutes.js";
+import noteRoutes from "./app/routes/noteRoutes.js";
+import categoryRoutes from "./app/routes/categoryRoutes.js";
 
 //load .env file to process.env
 dotenv.config();
@@ -19,6 +19,12 @@ app.use(express.json());
 app.use("/api/notes", noteRoutes);
 
 app.use("/api/categories", categoryRoutes);
+
+//404 Error, if the url not found
+app.use(notFound);
+
+//Error Handling
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
